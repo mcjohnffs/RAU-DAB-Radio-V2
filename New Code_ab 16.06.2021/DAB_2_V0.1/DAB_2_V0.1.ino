14004b76d1fc082a6a41571e6f4f143d78935b81
mcjohnffs <mcjohnffs@gmail.com>
2021-06-21 23:17:35 +0200

    Update 21.06
    -bm83/64 test code
    -mcp2 code implemented

--- New Code_ab 16.06.2021/DAB_2_V0.1/DAB_2_V0.1.ino
+++ New Code_ab 16.06.2021/DAB_2_V0.1/DAB_2_V0.1.ino
@@ -8,12 +8,12 @@
  * generating source code documentation with Doxygen.
  *
  * @section circuit Produkt
- * - Red LED connected to pin D2.
- * - Momentary push button connected to pin D3.
+ * - 1
+ * - 2
  *
  * @section libraries Libraries
- * - Arduino_LSM6DS3 (https://github.com/arduino-libraries/Arduino_LSM6DS3)
- *   - Interacts with on-board IMU.
+ * - 2
+ *   - 1
  *
  * @section notes Notizen
  * - Comments are Doxygen compatible.
@@ -27,7 +27,7 @@
  *
  * Copyright (c) RAU Regionales Ausbildungszentrum AU.  All rights reserved.
  */
- 
+
 // Libraries
 #include <lvgl.h>
 #include <TFT_eSPI.h>
@@ -35,17 +35,29 @@
 #include <Wire.h>
 #include <PCA9634.h>
 #include <MCP23017.h>
+#include "BM83.h"           // Bluetooth Module BM83 Library
+#include <SoftwareSerial.h> //SoftwareSerial Library for ESP32
+#include <BD37544FS.h>
 #include "soc/timer_group_struct.h"
 #include "soc/timer_group_reg.h"
 
 // Defines
 #define BUFFER_MULTIPLIER 35
-
+#define RX_PIN 0
+#define TX_PIN 4
+#define TX_IND 5
+const int mfbPin = 23;
 TaskHandle_t Task1;
+TaskHandle_t Task2;
+
+BD37544FS bd;
+SoftwareSerial swSerial(RX_PIN, TX_PIN);
+BM83 bm83(swSerial, TX_IND);
 
 PCA9634 ledDriver(0x15, 4);
 
-MCP23017 mcp = MCP23017(0x20);
+MCP23017 mcp1 = MCP23017(0x20);
+MCP23017 mcp2 = MCP23017(0x24);
 
 // Global LVGL objects
 lv_obj_t *tabview;
@@ -107,7 +119,8 @@ void setup()
 
   Wire.begin();
 
-  Serial.begin(115200);
+  Serial.begin(9600);
+  swSerial.begin(115200);
 
   xTaskCreatePinnedToCore(
       read_inputs,   /* Task function. */
@@ -118,19 +131,36 @@ void setup()
       &Task1,        /* Task handle. */
       0);            /* Core */
 
+  xTaskCreatePinnedToCore(
+      bm83_loop,   /* Task function. */
+      "bm83_loop", /* String with name of task. */
+      20000,       /* Stack size in bytes. */
+      NULL,        /* Parameter passed as input of the task */
+      1,           /* Priority of the task. */
+      &Task2,      /* Task handle. */
+      0);          /* Core */
+
   while (!Serial)
     ;
 
-  mcp.init();
+  mcp1.init();
+  mcp1.portMode(MCP23017Port::A, 0b11111111);
+  delay(5);
 
-  mcp.portMode(MCP23017Port::A, 0b11111111);
+  mcp2.init();
+  mcp2.portMode(MCP23017Port::A, 0b00000111);
+  delay(5);
 
+  mcp2.writePort(MCP23017Port::A, 0b11000000);
+  Serial.println("7.5V enabled!");
+
+delay(5);
   lv_init();
 
 #if USE_LV_LOG != 0
   lv_log_register_print_cb(my_print); /* register print function for debugging */
 #endif
-
+delay(5);
   // TFT initialization ===========================
   tft.begin();
   tft.setRotation(3);
@@ -152,6 +182,15 @@ void setup()
   ledDriver.begin();
 
   ledDriver.allOff();
+
+  pinMode(mfbPin, OUTPUT);    // sets the MFB Pin 4 as output
+  digitalWrite(mfbPin, HIGH); // sets the MFB Pin 4 "High" to power on BM83 over BAT_IN
+  delay(500);
+
+  bm83.run();
+  bm83.powerOn();                                    // Sends "power on" command over UART to BM83
+  bm83.mmiAction(BM83_MMI_STANDBY_ENTERING_PAIRING); // Sends "enter Pairing" command over UART to BM83
+  digitalWrite(mfbPin, LOW);                         // sets the MFB Pin 4 "LOW" (no longer needed after power on process)
 
   lv_disp_buf_init(&disp_buf, buf_1, buf_2, LV_HOR_RES_MAX * 10);
 
@@ -233,7 +272,6 @@ void setup()
   ser8 = lv_chart_add_series(chart, LV_COLOR_AQUA);
 */
 
-
   /*Create a Preloader object*/
   lv_obj_t *preload = lv_spinner_create(tab4, NULL);
   lv_obj_set_size(preload, 100, 100);
@@ -248,6 +286,17 @@ void setup()
   lv_gauge_set_value(gauge1, 0, 10);
   lv_gauge_set_value(gauge1, 1, 20);
   lv_gauge_set_value(gauge1, 2, 30);
+
+  bd.setSelect(0);  // int 0...7 === A B C D E F INPUT_SHORT INPUT_MUTE
+  bd.setIn_gain(0); // int 0...7 === 0...20 dB
+  bd.setVol_1(-25); // int 0...87 === 0...-87 dB
+  bd.setFad_1(0);   // int 0...87 === 0...-87 dB
+  bd.setFad_2(0);   // int 0...87 === 0...-87 dB
+  bd.setBass(0);    // int -7...0...+7 === -14...+14 dB
+  bd.setMidd(0);    // int -7...0...+7 === -14...+14 dB
+  bd.setTreb(0);    // int -7...0...+7 === -14...+14 dB
+
+  Serial.println("Sound processor configured!");
 }
 
 void loop()
@@ -255,7 +304,7 @@ void loop()
   TIMERG0.wdt_wprotect = TIMG_WDT_WKEY_VALUE;
   TIMERG0.wdt_feed = 1;
   TIMERG0.wdt_wprotect = 0;
-  conf = mcp.readRegister(MCP23017Register::GPIO_A);
+  conf = mcp1.readRegister(MCP23017Register::GPIO_A);
 
   lv_task_handler();
   delay(3);
@@ -305,6 +354,58 @@ void read_inputs(void *parameter)
 
           break;
         }
+      }
+    }
+  }
+  vTaskDelay(5);
+}
+
+void bm83_loop(void *pvParameters)
+{
+  for (;;)
+  {
+
+    //BM83 start routine (required)1
+
+    if (Serial.available() > 0)
+    {
+
+      // read the incoming byte:
+      char c = Serial.read();
+      switch (c)
+      {
+      case 'a':
+        bm83.getStatus();
+        break;
+      case 'b':
+        // Other Action, Please see BM83_Debug.h
+        bm83.musicControl(MUSIC_CONTROL_PLAY);
+        break;
+      case 'c':
+        // Other Action, Please see BM83_Debug.h
+        bm83.musicControl(MUSIC_CONTROL_NEXT);
+        break;
+      case 'd':
+        bm83.startBt();
+        break;
+      case 'e':
+        // Other Action, Please see BM83_Debug.h
+        bm83.mmiAction(BM83_MMI_STANDBY_ENTERING_PAIRING);
+        break;
+      case 'f':
+        // Other Action, Please see BM83_Debug.h
+        bm83.mmiAction(BM83_MMI_MUTE_MIC);
+        break;
+      case 'g':
+        bm83.powerOff();
+        break;
+      case 'h':
+        bm83.powerOn();
+        break;
+      case 'i':
+        String number_s = "1234567890";
+        bm83.makeCall(&number_s);
+        break;
       }
     }
   }
